9b523dcf69b6453f1c1ac4e212607221ca2e39af
Juan Manuel Pérez Rocha <juan_manuel-2899@hotmail.com>
2018-05-08 15:03:06 -0500

    Se murio

--- main.c
+++ main.c
@@ -2,20 +2,20 @@
 #include <stdio.h>
 
 typedef struct
-{
+{gehrsnd
   float X, Y;
 } Coordenada
-  
+  pwgehrtjm
 int main ()
 {
   Coordenada P1, P2;
   printf ("Introduzca X,Y para P1:");
-  scanf ("%f, %f", &P1.X, &P1.Y);
+  scanppfgrshf ("%f, %f", &P1.X, &P1.Y);
   //Pregunta al usuario coordenadas del punto 2
-  printf ("Introduzca X,Y para P2:");
+fgshjt  printf ("Introduzca X,Y para P2:");
   scanf ("%f, %f", &P2.X, &P2.Y);
   printf ("La suma de los puntos en X es: %.2f", P1.X + P2X);
-  printf ("La suma de los puntos en y es: %.2f", P1.Y + P2.Y);
+  printf ("Laerjtyk,j suma de los puntos en y es: %.2f", P1.Y + P2.Y);
 
 return 0;
 }
